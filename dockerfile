c744056c43fe31f95d51b3172e9da54d3818856e
tok <tok@frogge.gay>
2026-01-08 05:24:52 -0600

    png support added via /icon.png

--- dockerfile
+++ dockerfile
@@ -4,10 +4,6 @@ WORKDIR /app/
 
 COPY * .
 
-#RUN chmod 0744 ./upload.sh
-
-
 RUN pip install -r ./requirements.txt
-
 
 CMD [ "fastapi", "run", "/app/server.py", "--port", "8080", "--proxy-headers" ]
--- server.py
+++ server.py
@@ -5,7 +5,12 @@ from .makepfp import draw_default_clock
 
 app:FastAPI = FastAPI()
 @app.get("/")
-def get_pfp_png():
+def get_pfp():
     clock:dw.Drawing= draw_default_clock()
     resp:Response  = Response(clock.as_svg(), media_type="image/svg+xml")
+    return resp
+@app.get("/icon.png")
+def get_pfp_png():
+    clock:dw.Drawing= draw_default_clock()
+    resp:Response  = Response(clock.rasterize().png_data, media_type="image/png")
     return resp
